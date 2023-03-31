3b3309b43df272b89060d5326358c6ebb48cb63e
VINAY BHARTI <vinaybharti1996@gmail.com>
2023-03-31 16:19:39 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+
+#import library
+import random
+
+#define combinations
+lowers = 'abcdefghijklmnopqrstuvwxyz'
+uppers = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '012345678'
+symbols = '!@#$%^&*'
+
+combination = lowers + uppers + numbers + symbols
+
+#fix the length of password
+length = 8
+
+#generating the password
+password = ''.join(random.sample(combination, length))
+
+#here the password you got!
+print(password) 
